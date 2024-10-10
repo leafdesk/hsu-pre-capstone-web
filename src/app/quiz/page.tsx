7e21383ed79a064bb31e088ce15e5502583d1a79
leafdesk <lsg001008@gmail.com>
2024-10-10 12:56:36 +0900

    프로젝트 초기 세팅

---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const QuizPage = () => {
+  return (
+    <>
+      <>여기는 퀴즈 페이지</>
+
+      <div>퀴즈 화면 구현 예정....</div>
+    </>
+  );
+};
+
+export default QuizPage;
---
+++ src/app/summary/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const SummaryPage = () => {
+  return (
+    <>
+      <>여기는 요약본 보는 페이지</>
+
+      <div>요약본 데이터....</div>
+    </>
+  );
+};
+
+export default SummaryPage;
