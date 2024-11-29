c4be2aa4fc27bed1f41c901512de856ff6904682
leafdesk <lsg001008@gmail.com>
2024-11-29 10:46:40 +0900

    🚧 [WIP] 2, 3번 회의 작업 중 - 퀴즈

--- src/app/[meetingId]/quiz/page.tsx
+++ src/app/[meetingId]/quiz/page.tsx
@@ -1,7 +1,8 @@
 'use client'
+import { usePathname } from 'next/navigation'
 import { useState, useEffect } from 'react'
 
-const quizQuestions = [
+const quizQuestions1 = [
   {
     question: '이번 회의는 몇 주차 프로젝트 진행상황 점검 회의였나요?',
     options: ['11주차', '12주차', '13주차', '14주차'],
@@ -40,11 +41,115 @@ const quizQuestions = [
   },
 ]
 
+const quizQuestions2 = [
+  {
+    question: '프리캡스톤 전시회 준비 회의에서 행사 준비 장소는 어디인가요?',
+    options: ['체육관 1층', '상상관 체육관', '대운동장', '강당'],
+    correctAnswer: 1,
+  },
+  {
+    question:
+      '행사 준비 시간 동안 상주 인원 확보 문제의 주요 원인은 무엇인가요?',
+    options: [
+      '모두 수업으로 인해 오전에 시간 부족',
+      '행사 장소 변경으로 인한 혼란',
+      '포스터 작업 지연',
+      '참가자들의 행사 불참',
+    ],
+    correctAnswer: 0,
+  },
+  {
+    question: '포스터 작업은 언제 완료될 예정인가요?',
+    options: ['수요일', '목요일', '금요일', '토요일'],
+    correctAnswer: 2,
+  },
+  {
+    question: '행사 일정 조율에서 가장 늦게 가능한 시간은 언제인가요?',
+    options: ['11시', '12시', '3시', '5시'],
+    correctAnswer: 2,
+  },
+  {
+    question: '프리캡스톤 전시회 준비 회의의 주요 결정 사항이 아닌 것은?',
+    options: [
+      '29일 금요일 일정 확정',
+      '포스터 인쇄 완료',
+      '프로젝트 발표 자료 준비',
+      '다른 팀과 공동 부스 운영',
+    ],
+    correctAnswer: 3,
+  },
+]
+
+const quizQuestions3 = [
+  {
+    question:
+      '크리스마스 및 연말 일정 관련 회의에서 김정훈이 언급한 크리스마스 이브의 주요 활동은 무엇인가요?',
+    options: [
+      '친구들과 외출',
+      '대타로 인해 일함',
+      '가족과 함께 시간 보냄',
+      '교회 예배 참석',
+    ],
+    correctAnswer: 1,
+  },
+  {
+    question: '조민서의 크리스마스 계획은 무엇인가요?',
+    options: [
+      '교회 예배 참석 후 친구들과 만남',
+      '크리스마스 마켓 방문',
+      '가족과 여행',
+      '연인과 데이트',
+    ],
+    correctAnswer: 0,
+  },
+  {
+    question: '회의에서 날씨와 관련하여 강조된 점은 무엇인가요?',
+    options: [
+      '눈이 올 예정',
+      '온화한 날씨 예상',
+      '비 소식',
+      '강추위로 인한 행사 연기 가능성',
+    ],
+    correctAnswer: 0,
+  },
+  {
+    question: '오승민이 계획한 연말 활동은 무엇인가요?',
+    options: [
+      '교회 예배 참석',
+      '크리스마스 마켓 방문',
+      '연말 파티 준비',
+      '집에서 휴식',
+    ],
+    correctAnswer: 1,
+  },
+  {
+    question: '크리스마스 및 연말 일정 회의의 주요 결정 사항이 아닌 것은?',
+    options: [
+      '건강 관리 중요성 강조',
+      '크리스마스 마켓 방문 논의',
+      '연말 일정 각자 조율',
+      '팀 전원의 크리스마스 행사 참여 확정',
+    ],
+    correctAnswer: 3,
+  },
+]
+
 /**
  * 퀴즈 페이지.
  */
 const QuizPage = () => {
   const [currentQuestion, setCurrentQuestion] = useState(1)
+
+  const pathname = usePathname()
+  const pathSegments = pathname.split('/')
+  const meetingId = pathSegments[1]
+  const quizQuestions =
+    meetingId === '1'
+      ? quizQuestions1
+      : meetingId === '2'
+      ? quizQuestions2
+      : quizQuestions3
+
   const totalQuestions = quizQuestions.length
   const [timeLeft, setTimeLeft] = useState(10) // 타이머를 10초로 설정
   const [showResult, setShowResult] = useState(false) // 결과 모달 표시 여부
